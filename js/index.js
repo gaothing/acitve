9f992a6bcd62817ced06f18aff40c36ce1031003
gaothing <744989745@qq.com>
2018-08-31 14:30:16 +0800

    'sdf'

---
+++ js/index.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+	$(".actOne").addClass("actOnego")
+	var win = $(window); 
+	var sc = $(document); 
+	$(window).scroll(function() {
+		if(sc.scrollTop() > $(".actTwo").offset().top - 300) {
+			$(".actTwo").addClass("actTwogo")
+		}
+		if(sc.scrollTop() > $(".actThree").offset().top - 300) {
+			$(".actThree").addClass("actThreego")
+		}
+		if(sc.scrollTop() > $(".actFour").offset().top - $('.actFour').height()-100) {
+			$(".actFour").addClass("actFourgo")
+		}
+	});
+}
